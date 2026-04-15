4c37c0d0408567c6d293b568bca35c5064ea9558
th100129 <th100129@gmail.com>
2026-04-15 15:15:34 +0900

    qa dataset falt, train

---
+++ scripts/__init__.py
@@ -0,0 +1,13 @@
+from .postprocess_2507 import (
+    Postprocess2507Config,
+    Qwen2507Postprocessor,
+    load_postprocess_config,
+    postprocess_with_2507,
+)
+
+__all__ = [
+    "Postprocess2507Config",
+    "Qwen2507Postprocessor",
+    "load_postprocess_config",
+    "postprocess_with_2507",
+]
